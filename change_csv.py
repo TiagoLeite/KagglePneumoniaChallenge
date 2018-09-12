52df0f6307472039fa909c25c766f5c63e2a34ce
TiagoLeite <tiago.tmleite@gmail.com>
2018-09-12 15:05:34 -0300

    new file, change csv

---
+++ change_csv.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+data = pd.read_csv('resultados_mobilenet_v1_0164.csv')
+
+THRESHOLD = 0.5  # ----->>>>>> change this according to your preference
+
+new_strings = list()
+
+for line in data['PredictionString']:
+    if type(line) is str:
+        tokens = line.split(' ')
+        new_line = ""
+        for k in range(int(len(tokens)/5)):
+            if float(tokens[k * 5]) >= THRESHOLD:
+                for i in range(k * 5, k * 5 + 5):
+                    new_line += tokens[i] + " "
+        new_strings.append(new_line)
+    else:
+        new_strings.append("")
+
+
+new_file = pd.DataFrame({'patientId': data['patientId'], 'PredictionString': new_strings})
+new_file.to_csv('new_results.csv', index=False)
